b24f49a968d5c8a2732e648beeaf17c7bb0221e8
nikova23 <nikova_n@abv.bg>
2022-06-11 16:51:10 +0300

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
 
     public static void printSamenumbers () {
-        int[] numbers  = {2,16,7,8,16,17,5,2} ;
+        int[] numbers  = {2,16,7,8,16,17,5,2,7} ;
         System.out.println(numbers[0] );
         System.out.println(numbers[1]);
         for (int i = 0; i < numbers.length-1 ; i++) {
             System.out.println(i);
+
             int[] numbers1  = {2,16,7,8,16,17,5,2} ;
            for (int j = 0; j < numbers1.length -1;j++) {
                 System.out.println(i==j);
